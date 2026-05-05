de35628e008eb0de2004930c59bb451647ac65f4
Mamatha M <mamatharajkethavath@gmail.com>
2026-05-05 10:07:01 -0600

    function call

---
+++ Functions/ex2.py
@@ -0,0 +1,7 @@
+def display_message(message):
+    print(f"Learning {message.title()}")
+display_message('python')
+
+def favorite_book(book):
+    print(f"Favorite book {book.title()}")
+favorite_book('python')
